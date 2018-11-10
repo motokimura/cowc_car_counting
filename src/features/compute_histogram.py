8e49379a68c169c444154238db8c4aa0e618a528
motokimura <motokimura1@gmail.com>
2018-11-10 23:34:10 +0900

    Add compute_histogram.py

---
+++ src/features/compute_histogram.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+import argparse
+import os
+import numpy as np
+from tqdm import tqdm
+
+import sys
+sys.path.append("../models")
+
+from dataset import CowcDataset_Counting
+
+
+def compute_histogram(dataset):
+    
+    hist = np.zeros(shape=[10**3, ], dtype=int)
+    
+    for image, label in tqdm(dataset):
+        
+        hist[label] += 1
+    
+    car_count_max = np.where(hist > 0)[0][-1]
+    
+    return hist[:car_count_max + 1]
+
+
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser(description='Compute images mean array')
+	
+	parser.add_argument('--data_list', help='Path to training image-label list file',
+						default='../../data/cowc_processed/train_val/crop/train.txt')
+	parser.add_argument('--root', help='Root directory path of image files', 
+						default='../../data/cowc_processed/train_val/crop/data')
+	parser.add_argument('--output', help='path to output distriba array',
+						default='../../data/cowc_processed/train_val/crop/histogram.npy')
+
+	args = parser.parse_args()
+
+	dataset = CowcDataset_Counting(args.data_list, args.root)
+	hist = compute_histogram(dataset)
+
+	print("Computed histogram:")
+	print("car_num, count")
+	for car_num, count in enumerate(hist):
+		print("{}, {}".format(car_num, count))
+		
+	np.save(args.output, hist)
